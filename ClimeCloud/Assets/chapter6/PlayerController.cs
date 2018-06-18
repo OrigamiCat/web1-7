85d9761ce44539e7a1f5cf79b22108212d3b3469
Unknown <aiyuixps13@outlook.jp>
2018-06-18 17:57:21 +0900

    コミット

--- ClimeCloud/Assets/chapter6/PlayerController.cs
+++ ClimeCloud/Assets/chapter6/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
     Rigidbody2D rigid2D;
@@ -16,8 +17,9 @@ public class PlayerController : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.Space))
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0)
         {
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
@@ -28,7 +30,8 @@ public class PlayerController : MonoBehaviour {
 
         float speedx = Mathf.Abs(this.rigid2D.velocity.x);
 
-        if(speedx < this.maxWalkSpeed){
+        if (speedx < this.maxWalkSpeed)
+        {
             this.rigid2D.AddForce(transform.right * key * this.workForce);
         }
 
@@ -39,5 +42,16 @@ public class PlayerController : MonoBehaviour {
 
 
         this.animator.speed = speedx / 2.0f;
+
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
     }
+        void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("ゴール");
+        SceneManager.LoadScene("ClearScene");
+    }
+
 }
